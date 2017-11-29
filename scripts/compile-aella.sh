8fedf133a65688f2298de6a7703fa5dffa9eccb4
Céline de Roland <celinederoland@itpassion.info>
2017-11-29 08:56:57 +0100

    add installation script + tomcat server + maven compilator container

---
+++ scripts/compile-aella.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd $DELPH_PATH/titan
+docker-compose up aella-code
---
+++ scripts/install-titan.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -s $DELPH_PATH/scripts/clean-docker.sh /usr/local/bin/titan-clean-docker
+ln -s $DELPH_PATH/scripts/compile-aella.sh /usr/local/bin/titan-compile-aella
+ln -s $DELPH_PATH/scripts/install-blog.sh /usr/local/bin/titan-install-blog
+ln -s $DELPH_PATH/scripts/update-blog.sh /usr/local/bin/titan-update-blog
+ln -s $DELPH_PATH/scripts/update-remote.sh /usr/local/bin/titan-update-remote
+ln -s $DELPH_PATH/scripts/update-titan.sh /usr/local/bin/titan-update-titan
